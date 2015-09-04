4ea733f786c906ea2d916cee6a7342a2aa128c02
Thomas Stuppy <thomas.stuppy@gmail.com>
2015-09-04 22:52:32 +0200

    init

---
+++ validate.php
@@ -0,0 +1,201 @@
+<?php
+
+# FORMULARDATEN
+$result = array (
+    # Allg. Abschn. 1
+    "a1"  => array(
+        #1
+        "1-1-a" => array($_POST["1-1-a"], 4),
+        "1-1-b" => array($_POST["1-1-b"], 5),
+        "1-1-c" => array($_POST["1-1-c"], 3),
+        "1-1-d" => array($_POST["1-1-d"], 2),
+        "1-1-e" => array($_POST["1-1-e"], 1),
+        #2
+        "1-2-a" => array($_POST["1-2-a"], 3),
+        "1-2-b" => array($_POST["1-2-b"], 5),
+        "1-2-c" => array($_POST["1-2-c"], 4),
+        "1-2-d" => array($_POST["1-2-d"], 1),
+        "1-2-e" => array($_POST["1-2-e"], 2),
+        #3
+        "1-3-a" => array($_POST["1-3-a"], 3),
+        "1-3-b" => array($_POST["1-3-b"], 2),
+        "1-3-c" => array($_POST["1-3-c"], 1),
+        "1-3-d" => array($_POST["1-3-d"], 4),
+        "1-3-e" => array($_POST["1-3-e"], 2)
+    ),
+    # Allg. Abschn. 2
+    "a2" => array(
+        #4
+        "1-4-a" => array($_POST["1-4-a"], 6),
+        "1-4-b" => array($_POST["1-4-b"], 1),
+        "1-4-c" => array($_POST["1-4-c"], 1),
+        "1-4-d" => array($_POST["1-4-d"], 6),
+        #5
+        "1-5-a" => array($_POST["1-5-a"], 1),
+        "1-5-b" => array($_POST["1-5-b"], 6),
+        "1-5-c" => array($_POST["1-5-c"], 1),
+        "1-5-d" => array($_POST["1-5-d"], 1),
+        "1-5-e" => array($_POST["1-5-e"], 6),
+        "1-5-f" => array($_POST["1-5-f"], 1),
+        #6 <------------------------
+        "1-6-a" => array($_POST["1-6-a"], 2),
+        "1-6-b" => array($_POST["1-6-b"], 3),
+        "1-6-c" => array($_POST["1-6-c"], 5),
+        "1-6-d" => array($_POST["1-6-d"], 4),
+        "1-6-e" => array($_POST["1-6-e"], 1),
+        "1-6-f" => array($_POST["1-6-f"], 6)
+    ),
+    # Allg. Abschn. 3
+    "a3"   => array(
+        #7
+        "1-7-a" => array($_POST["1-7-a"], 1),
+        "1-7-b" => array($_POST["1-7-b"], 3),
+        "1-7-c" => array($_POST["1-7-c"], 3),
+        "1-7-d" => array($_POST["1-7-d"], 2),
+        "1-7-e" => array($_POST["1-7-e"], 5),
+        "1-7-f" => array($_POST["1-7-f"], 4),
+        "1-7-g" => array($_POST["1-7-g"], 2),
+        #8
+        "1-8-a" => array($_POST["1-8-a"], 1),
+        "1-8-b" => array($_POST["1-8-b"], ""),
+        "1-8-c" => array($_POST["1-8-c"], ""),
+        "1-8-d" => array($_POST["1-8-d"], ""),
+        "1-8-e" => array($_POST["1-8-e"], ""),
+        "1-8-f" => array($_POST["1-8-f"], 1),
+        "1-8-g" => array($_POST["1-8-g"], 1),
+        #9
+        "1-9-a" => array($_POST["1-9-a"], 2),
+        "1-9-b" => array($_POST["1-9-b"], 3),
+        "1-9-c" => array($_POST["1-9-c"], 4),
+        "1-9-d" => array($_POST["1-9-d"], 1),
+        "1-9-e" => array($_POST["1-9-e"], 5)
+    ),
+    # Mathe Abschn. 1
+    "m1"   => array(
+        #10
+        "1-10-a" => array($_POST["1-10-a"], 4.25),
+        "1-10-b" => array($_POST["1-10-b"], 1.2),
+        "1-10-c" => array($_POST["1-10-c"], 4.00),
+        "1-10-d" => array($_POST["1-10-d"], 8.4),
+        #11
+        "1-11-a" => array($_POST["1-11-a"], 92.05),
+        "1-11-b" => array($_POST["1-11-b"], 99.06),
+        "1-11-c" => array($_POST["1-11-c"], 99.65),
+        "1-11-d" => array($_POST["1-11-d"], 96.75),
+        #12
+        "1-12"   => array($_POST["1-12"], 2.50),
+        #13
+        "1-13-a" => array($_POST["1-13-a"], 03),
+        "1-13-b" => array($_POST["1-13-b"], "19:01")
+    ),
+    # Mathe Abschn. 2
+    "m2"   => array(
+        #14
+        "1-14-a" => array($_POST["1-14-a"], 0.85),
+        "1-14-b" => array($_POST["1-14-b"], 0.90),
+        "1-14-c" => array($_POST["1-14-c"], 3.30),
+        #15
+        "1-15-a" => array($_POST["1-15-a"], 4.27),
+        "1-15-b" => array($_POST["1-15-b"], 6.44),
+        #16
+        "1-16-a" => array($_POST["1-16-a"], 104),
+        "1-16-b" => array($_POST["1-16-b"], 581),
+        "1-16-c" => array($_POST["1-16-c"], 1)
+    ),
+    # Deutsch Abschn. 1
+    "d1"   => array(
+        #17
+        "1-17-a" => array($_POST["1-17-a"], 2),
+        "1-17-b" => array($_POST["1-17-b"], 3),
+        "1-17-c" => array($_POST["1-17-c"], 3),
+        "1-17-d" => array($_POST["1-17-d"], 2),
+        "1-17-e" => array($_POST["1-17-e"], 2),
+        "1-17-f" => array($_POST["1-17-f"], 1),
+        #18
+        "1-18-a" => array($_POST["1-18-a"], 9),
+        "1-18-b" => array($_POST["1-18-b"], 6),
+        "1-18-c" => array($_POST["1-18-c"], 1),
+        "1-18-d" => array($_POST["1-18-d"], 14),
+        "1-18-e" => array($_POST["1-18-e"], 1)
+    ),
+    # Deutsch Abschn. 2
+    "d2"   => array(
+        #19
+        "1-19-2"  => array($_POST["1-19-2"], "nehmen"),
+        "1-19-3"  => array($_POST["1-19-3"], "diesj&auml;hrigen"),
+        "1-19-4"  => array($_POST["1-19-4"], "n&auml;mlich"),
+        "1-19-5"  => array($_POST["1-19-5"], "teilnehmen"),
+        "1-19-6"  => array($_POST["1-19-6"], "Geb&auml;ude"),
+        "1-19-7"  => array($_POST["1-19-7"], "bequeme"),
+        "1-19-8"  => array($_POST["1-19-8"], "viel"),
+        "1-19-9"  => array($_POST["1-19-9"], "lassen"),
+        "1-19-10" => array($_POST["1-19-10"], "Sp&uuml;ren"),
+        "1-19-11" => array($_POST["1-19-11"], "Zuschauen"),
+        "1-19-12" => array($_POST["1-19-12"], "Strand-Bar"),
+        "1-19-13" => array($_POST["1-19-13"], "wieder"),
+        "1-19-14" => array($_POST["1-19-14"], "Interesse"),
+        "1-19-15" => array($_POST["1-19-15"], "bitte"),
+        "1-19-16" => array($_POST["1-19-16"], ""),
+        "1-19-17" => array($_POST["1-19-17"], "Teilnahme"),
+        "1-19-18" => array($_POST["1-19-18"], "Organisationsteam"),
+        "1-19-19" => array($_POST["1-19-19"], "Fragen"),
+        "1-19-20" => array($_POST["1-19-20"], "")
+    ),
+    # Deutsch Abschn. 3
+    "d3"   => array(
+        #20
+        "1-20-a" => array($_POST["1-20-a"], 2),
+        "1-20-b" => array($_POST["1-20-b"], 1),
+        "1-20-c" => array($_POST["1-20-c"], 3),
+        "1-20-d" => array($_POST["1-20-d"], 5),
+        "1-20-e" => array($_POST["1-20-e"], 4),
+        "1-20-f" => array($_POST["1-20-f"], 6),
+        "1-20-g" => array($_POST["1-20-g"], 9),
+        "1-20-h" => array($_POST["1-20-h"], 7),
+        "1-20-i" => array($_POST["1-20-i"], 8),
+        #21
+        "1-21-a" => array($_POST["1-21-a"], 3),
+        "1-21-b" => array($_POST["1-21-b"], 2),
+        "1-21-c" => array($_POST["1-21-c"], 3),
+        "1-21-d" => array($_POST["1-21-d"], 1),
+        "1-21-e" => array($_POST["1-21-e"], 3)
+    )
+);
+
+$allgemein = array(
+    "fp1" => 0,
+    "fp2" => 0,
+    "fp3" => 0,
+    "sum" => 0,
+    "max" => 50
+);
+$mathe = array(
+    "m1" => 0,
+    "m2" => 0,
+    "sum" => 0,
+    "max" => 38
+);
+$deutsch = array(
+    "d1" => 0,
+    "d2" => 0,
+    "d3" => 0,
+    "sum" => 0,
+    "max" => 44
+);
+
+foreach($result["a1"] as $a1) {
+    if($a1["0"] != $a1["1"]) $allgemein["fp1"]++;
+}
+foreach($result["a2"] as $a2) {
+    if($a2["0"] != $a2["1"]) $allgemein["fp2"]++;
+}
+foreach($result["a3"] as $a3) {
+    if($a3["0"] != $a3["1"]) $allgemein["fp3"]++;
+}
+
+$allgemein["sum"] = $allgemein["fp1"] + $allgemein["fp2"] + $allgemein["fp3"];
+
+
+var_dump($allgemein);
+
+?>
